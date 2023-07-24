1eafb6ca5075134f85dfe2a9a42edf9198552b35
gykunju <alvingikunju@gmail.com>
2023-07-24 14:14:31 +0300

    completed assignment

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
 # Write your code here
+require 'json'
+require 'net/http'
+require 'open-uri'
+
+class GetRequester
+     
+    def initialize url
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+
+        if response.is_a?(Net::HTTPSuccess)
+            response.body
+        else
+            raise "Error: Unable to retrieve data from the provided URL."
+        end
+    end
+
+    def parse_json
+        JSON.parse(self.get_response_body)
+    end
+end
